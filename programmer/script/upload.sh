f7614ef8eeb49fa423fbc9ab870b5cd4c754b503
Amir Bawab <amirbawab@gmail.com>
2017-07-18 16:25:44 -0400

    Enhanced upload.sh

--- programmer/script/upload.sh
+++ programmer/script/upload.sh
@@ -5,28 +5,28 @@
 # Used to flash a C program to the Atmega328p
 function flash() {
 
-    if [[ ${#@} -ne 1 ]]
+    if [[ -z "$1" ]]
     then
         cat<<EOF
-Flash a C program to the Atmega328p.
+Compile C program and upload generated hex to connected Atmega328p
 
 Usage: 
     ./upload.sh program.c
 EOF
     else
         # remove extension from arg
-        filename=$(echo $1 | cut -d '.' -f 1)
+        filename=$(echo "$1" | cut -d '.' -f 1)
 
         # create object and hex files
-        avr-gcc -std=c11 -mmcu=atmega328 -O -o "$filename.o" $@
-        avr-objcopy -O ihex $filename.o $filename.hex
+        avr-gcc -std=c11 -mmcu=atmega328 -O -o "${filename}.o" "$1"
+        avr-objcopy -O ihex "${filename}.o" "${filename}.hex"
 
         # flash hex file to atmega
-        sudo avrdude -c usbtiny -p m328p -U flash:w:$filename.hex
+        sudo avrdude -c usbtiny -p m328p -U "flash:w:${filename}.hex"
 
         # remove object and hex files to keep dir clean
-        rm $filename.o $filename.hex
+        rm "${filename}.o" "${filename}.hex"
     fi
 }
 
-flash $1
+flash "$1"
